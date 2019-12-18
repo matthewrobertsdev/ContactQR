72e44a17eb4edddcaf38af504a7db38dfec32b40
Matt <31932179+MattNoName@users.noreply.github.com>
2019-12-17 23:41:09 -0500

    more work on adding a new contact

--- ContactQR/CreateContactViewController.swift
+++ ContactQR/CreateContactViewController.swift
@@ -12,7 +12,6 @@ import Contacts
 class CreateContactViewController: UIViewController {
 	@IBOutlet weak var firstNameTextField: UITextField!
 	@IBOutlet weak var lastNameTextField: UITextField!
-	@IBOutlet weak var companyTextField: UITextField!
 	@IBOutlet weak var mobilePhoneTextField: UITextField!
 	@IBOutlet weak var workPhone1TextField: UITextField!
 	@IBOutlet weak var workPhone2TextField: UITextField!
@@ -27,12 +26,18 @@ class CreateContactViewController: UIViewController {
 	@IBOutlet weak var urlWork2TextField: UITextField!
 	@IBOutlet weak var otherUrl1TextField: UITextField!
 	@IBOutlet weak var otherUrl2TextField: UITextField!
-	@IBOutlet weak var homeAddress1TextField: UITextField!
-	@IBOutlet weak var homeAddress2TextField: UITextField!
-	@IBOutlet weak var workAddress1TextField: UITextField!
-	@IBOutlet weak var workAddress2TextField: UITextField!
-	@IBOutlet weak var otherAddress1TextField: UITextField!
-	@IBOutlet weak var otherAddress2TextField: UITextField!
+	@IBOutlet weak var homeStreetTextField: UITextField!
+	@IBOutlet weak var homeCityTextField: UITextField!
+	@IBOutlet weak var homeStateTextField: UITextField!
+	@IBOutlet weak var homeZipTextField: UITextField!
+	@IBOutlet weak var workStreetTextField: UITextField!
+	@IBOutlet weak var workStateTextField: UITextField!
+	@IBOutlet weak var workCityTextField: UITextField!
+	@IBOutlet weak var workZipTextField: UITextField!
+	@IBOutlet weak var otherStreetTextField: UITextField!
+	@IBOutlet weak var otherCityTextField: UITextField!
+	@IBOutlet weak var otherStateTextField: UITextField!
+	@IBOutlet weak var otherZipTextField: UITextField!
 	@IBAction func cancel(_ sender: Any) {
 		self.navigationController?.popViewController(animated: true)
 	}
@@ -41,6 +46,90 @@ class CreateContactViewController: UIViewController {
 		if  !(firstNameTextField.text=="") {
 			contact.givenName=firstNameTextField.text ?? ""
 		}
+		if  !(lastNameTextField.text=="") {
+			contact.familyName=lastNameTextField.text ?? ""
+		}
+		if  !(mobilePhoneTextField.text=="") {
+			
+		let phone=CNPhoneNumber(stringValue: mobilePhoneTextField.text ?? "")
+			contact.phoneNumbers.append(CNLabeledValue<CNPhoneNumber>(label: "Mobile", value:phone))
+		}
+		if  !(workPhone1TextField.text=="") {
+		let phone=CNPhoneNumber(stringValue: workPhone1TextField.text ?? "")
+			contact.phoneNumbers.append(CNLabeledValue<CNPhoneNumber>(label: "Work", value: phone))
+		}
+		if  !(workPhone2TextField.text=="") {
+		let phone=CNPhoneNumber(stringValue: workPhone2TextField.text ?? "")
+			contact.phoneNumbers.append(CNLabeledValue<CNPhoneNumber>(label: "Work", value: phone))
+		}
+		if  !(homePhoneTextField.text=="") {
+			let phone=CNPhoneNumber(stringValue: homePhoneTextField.text ?? "")
+			contact.phoneNumbers.append(CNLabeledValue<CNPhoneNumber>(label: "Home", value: phone))
+		}
+		if  !(otherPhoneTextField.text=="") {
+			let phone=CNPhoneNumber(stringValue: otherPhoneTextField.text ?? "")
+			contact.phoneNumbers.append(CNLabeledValue<CNPhoneNumber>(label: "Other", value: phone))
+		}
+		if  !(homeEmailTextField.text=="") {
+		let email=NSString(string: homeEmailTextField.text ?? "")
+			contact.emailAddresses.append(CNLabeledValue<NSString>(label: "Home", value: NSString(string: email)))
+		}
+		if  !(workEmail1TextField.text=="") {
+		let email=NSString(string: workEmail1TextField.text ?? "")
+			contact.emailAddresses.append(CNLabeledValue<NSString>(label: "Work", value: email))
+		}
+		if  !(workEmail2TextField.text=="") {
+		let email=NSString(string: workEmail2TextField.text ?? "")
+			contact.emailAddresses.append(CNLabeledValue<NSString>(label: "Work", value: email))
+		}
+		if  !(otherEmailTextField.text=="") {
+		let email=NSString(string: otherEmailTextField.text ?? "")
+			contact.emailAddresses.append(CNLabeledValue<NSString>(label: "Other", value: NSString(string: email)))
+		}
+		if  !(urlHomeTextField.text=="") {
+		let url=NSString(string: urlHomeTextField.text ?? "")
+			contact.urlAddresses.append(CNLabeledValue<NSString>(label: "Home", value: NSString(string: url)))
+		}
+		if  !(urlWork1TextField.text=="") {
+		let url=NSString(string: urlWork1TextField.text ?? "")
+			contact.urlAddresses.append(CNLabeledValue<NSString>(label: "Work", value: url))
+		}
+		if  !(urlWork2TextField.text=="") {
+		let url=NSString(string: urlWork2TextField.text ?? "")
+			contact.urlAddresses.append(CNLabeledValue<NSString>(label: "Work", value: url))
+		}
+		if  !(otherUrl1TextField.text=="") {
+		let url=NSString(string: otherUrl1TextField.text ?? "")
+			contact.urlAddresses.append(CNLabeledValue<NSString>(label: "Other", value: url))
+		}
+		if  !(otherUrl2TextField.text=="") {
+		let url=NSString(string: otherUrl2TextField.text ?? "")
+			contact.urlAddresses.append(CNLabeledValue<NSString>(label: "Other", value: url))
+		}
+		if !(homeStreetTextField.text=="") || !(homeCityTextField.text=="") || !(homeStateTextField.text=="") || !(homeZipTextField.text=="") {
+		let address=CNMutablePostalAddress()
+			address.street=homeStreetTextField.text ?? ""
+			address.city=homeCityTextField.text ?? ""
+			address.city=homeStateTextField.text ?? ""
+			address.city=homeZipTextField.text ?? ""
+			contact.postalAddresses.append(CNLabeledValue<CNPostalAddress>(label: "Home", value: address))
+			}
+		if !(workStreetTextField.text=="") || !(workCityTextField.text=="") || !(workStateTextField.text=="") || !(workZipTextField.text=="") {
+		let address=CNMutablePostalAddress()
+			address.street=workStreetTextField.text ?? ""
+			address.city=workCityTextField.text ?? ""
+			address.city=workStateTextField.text ?? ""
+			address.city=workZipTextField.text ?? ""
+			contact.postalAddresses.append(CNLabeledValue<CNPostalAddress>(label: "Work", value: address))
+			}
+		if !(otherStreetTextField.text=="") || !(otherCityTextField.text=="") || !(otherStateTextField.text=="") || !(otherZipTextField.text=="") {
+		let address=CNMutablePostalAddress()
+			address.street=otherStreetTextField.text ?? ""
+			address.city=otherCityTextField.text ?? ""
+			address.city=otherStateTextField.text ?? ""
+			address.city=otherZipTextField.text ?? ""
+			contact.postalAddresses.append(CNLabeledValue<CNPostalAddress>(label: "Work", value: address))
+			}
 		ActiveContact.shared.contact=contact
 		NotificationCenter.default.post(name: .contactCreated, object: self)
 	}
@@ -51,7 +140,6 @@ class CreateContactViewController: UIViewController {
 			let notificationCenter = NotificationCenter.default
 			notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
 			notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: UIResponder.keyboardWillChangeFrameNotification, object: nil)
-			print("abcd")
 			//self.stackViewHeight.constant=900
 			//self.fieldsStackView.updateConstraints()
 		//}
