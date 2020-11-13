0f1f9e28a9552f9a1ebdab6990cc57418cb0cfab
Matt <31932179+MattNoName@users.noreply.github.com>
2020-11-12 22:27:20 -0500

    Changed app name to Air Contacts, changed app icon, and started using SplitScreenController for UI

---
+++ Air Contacts/MainSplitViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainSplitViewController.swift
+//  Contact QR
+//
+//  Created by Matt Roberts on 11/11/20.
+//  Copyright © 2020 Matt Roberts. All rights reserved.
+//
+
+import UIKit
+
+class MainSplitViewController: UISplitViewController, UISplitViewControllerDelegate {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		self.delegate = self
+		self.preferredDisplayMode = .primaryOverlay
+	}
+	func splitViewController(
+		_ splitViewController: UISplitViewController,
+		collapseSecondary secondaryViewController: UIViewController,
+		onto primaryViewController: UIViewController) -> Bool {
+		return true
+	}
+
+	@available(iOS 14.0, *)
+	func splitViewController(_ svc: UISplitViewController, topColumnForCollapsingToProposedTopColumn proposedTopColumn: UISplitViewController.Column) -> UISplitViewController.Column {
+		return .primary
+	}
+
+}
