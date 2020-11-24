bcf5b497d6157c9a672a804dbfb53a6b4dd76284
Matt <31932179+MattNoName@users.noreply.github.com>
2020-11-24 01:17:49 -0500

    Created CircularColorView and added it to SavedContactTableViewCell.

---
+++ Air Contacts/Views/CircularColorView.swift
@@ -0,0 +1,21 @@
+//
+//  CircularColorView.swift
+//  Air Contacts
+//
+//  Created by Matt Roberts on 11/24/20.
+//  Copyright © 2020 Matt Roberts. All rights reserved.
+//
+import UIKit
+class CircularColorView: UIView {
+	required init?(coder: NSCoder){
+		super.init(coder: coder)
+		layer.cornerRadius=frame.width/2
+	}
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+}
