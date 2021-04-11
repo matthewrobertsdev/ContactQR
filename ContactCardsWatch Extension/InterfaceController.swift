0807040d02fbb2450da5108504dcec4a7f16b878
Matt <31932179+MattNoName@users.noreply.github.com>
2021-04-11 00:20:04 -0400

    Start of watch extension

---
+++ ContactCardsWatch Extension/InterfaceController.swift
@@ -0,0 +1,27 @@
+//
+//  InterfaceController.swift
+//  ContactCardsWatch Extension
+//
+//  Created by Matt Roberts on 4/11/21.
+//  Copyright © 2021 Matt Roberts. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
