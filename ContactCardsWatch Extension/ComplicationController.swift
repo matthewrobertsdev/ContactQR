1e900e62a91ed7944ddf6af0024d05fdb7369aaa
Matt <31932179+MattNoName@users.noreply.github.com>
2021-04-16 02:13:44 -0400

    Start of adding complications

--- ContactCardsWatch Extension/ComplicationController.swift
+++ ContactCardsWatch Extension/ComplicationController.swift
@@ -5,30 +5,22 @@
 //  Created by Matt Roberts on 4/11/21.
 //  Copyright © 2021 Matt Roberts. All rights reserved.
 //
-
 import ClockKit
-
-
 class ComplicationController: NSObject, CLKComplicationDataSource {
-    
     // MARK: - Complication Configuration
-
     func getComplicationDescriptors(handler: @escaping ([CLKComplicationDescriptor]) -> Void) {
         let descriptors = [
-            CLKComplicationDescriptor(identifier: "complication", displayName: "Contact Cards", supportedFamilies: CLKComplicationFamily.allCases)
+			CLKComplicationDescriptor(identifier: "complication", displayName: "QR Code", supportedFamilies: [.circularSmall, .modularSmall, .utilitarianSmall, .extraLarge, .graphicCorner,
+																											  .graphicCircular, .graphicExtraLarge])
             // Multiple complication support can be added here with more descriptors
         ]
-        
         // Call the handler with the currently supported complication descriptors
         handler(descriptors)
     }
-    
     func handleSharedComplicationDescriptors(_ complicationDescriptors: [CLKComplicationDescriptor]) {
         // Do any necessary work to support these newly shared complication descriptors
     }
-
     // MARK: - Timeline Configuration
-    
     func getTimelineEndDate(for complication: CLKComplication, withHandler handler: @escaping (Date?) -> Void) {
         // Call the handler with the last entry date you can currently provide or nil if you can't support future timelines
         handler(nil)
@@ -42,8 +34,31 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     // MARK: - Timeline Population
     
     func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
+		var template: CLKComplicationTemplate?
+		switch complication.family {
+		case .circularSmall:
+			template=CLKComplicationTemplateCircularSmallSimpleImage(imageProvider: CLKImageProvider(onePieceImage: UIImage(named: "Circular") ?? UIImage()))
+		case .modularSmall:
+			template=CLKComplicationTemplateModularSmallSimpleImage(imageProvider: CLKImageProvider(onePieceImage: UIImage(named: "Modular") ?? UIImage()))
+		case .utilitarianSmall:
+			template=CLKComplicationTemplateUtilitarianSmallSquare(imageProvider: CLKImageProvider(onePieceImage: UIImage(named: "Utilitaraian") ?? UIImage()))
+		case .extraLarge:
+			template=CLKComplicationTemplateExtraLargeSimpleImage(imageProvider: CLKImageProvider(onePieceImage: UIImage(named: "Extra Large") ?? UIImage()))
+		case .graphicCorner:
+			template=CLKComplicationTemplateGraphicCornerCircularImage(imageProvider: CLKFullColorImageProvider(fullColorImage: UIImage(named: "Graphic Corner") ?? UIImage()))
+		case .graphicCircular:
+			template=CLKComplicationTemplateGraphicCircularImage(imageProvider: CLKFullColorImageProvider(fullColorImage: UIImage(named: "Graphic Circular") ?? UIImage()))
+		case .graphicExtraLarge:
+			template=CLKComplicationTemplateGraphicExtraLargeCircularImage(imageProvider: CLKFullColorImageProvider(fullColorImage: UIImage(named: "Graphic Extra Large") ?? UIImage()))
+		default:
+			break
+		}
         // Call the handler with the current timeline entry
-        handler(nil)
+		if let completedTemplate=template {
+			handler(CLKComplicationTimelineEntry(date: Date(), complicationTemplate: completedTemplate))
+		} else {
+			handler(nil)
+		}
     }
     
     func getTimelineEntries(for complication: CLKComplication, after date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
@@ -52,7 +67,6 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     }
 
     // MARK: - Sample Templates
-    
     func getLocalizableSampleTemplate(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
         // This method will be called once per supported complication, and the results will be cached
         handler(nil)
