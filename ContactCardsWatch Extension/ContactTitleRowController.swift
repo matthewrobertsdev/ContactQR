86ac147784ca21879e5e726a5ccd4a492b38b823
Matt <31932179+MattNoName@users.noreply.github.com>
2021-05-28 16:12:40 -0400

    Organizing project files and got rid of some old ones.

--- ContactCardsWatch Extension/ContactTitleRowController.swift
+++ ContactCardsWatch Extension/ContactTitleRowController.swift
@@ -5,11 +5,8 @@
 //  Created by Matt Roberts on 5/21/21.
 //  Copyright © 2021 Matt Roberts. All rights reserved.
 //
-
 import WatchKit
-
 class ContactTitleRowController: NSObject {
 	@IBOutlet weak var group: WKInterfaceGroup!
 	@IBOutlet weak var titleLabel: WKInterfaceLabel!
-	
 }
--- ShowMyCardSirirUI/IntentViewController.swift
+++ ShowMyCardSirirUI/IntentViewController.swift
@@ -5,15 +5,11 @@
 //  Created by Matt Roberts on 5/26/21.
 //  Copyright © 2021 Matt Roberts. All rights reserved.
 //
-
 import IntentsUI
-
 // As an example, this extension's Info.plist has been configured to handle interactions for INSendMessageIntent.
 // You will want to replace this or add other intents as appropriate.
 // The intents whose interactions you wish to handle must be declared in the extension's Info.plist.
-
 // You can test this example integration by saying things to Siri like:
-// "Send a message using <myApp>"
 class IntentViewController: UIViewController, INUIHostedViewControlling {
     override func viewDidLoad() {
         super.viewDidLoad()
