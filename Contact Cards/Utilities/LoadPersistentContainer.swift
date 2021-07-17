2cf86e41568bac6e70149a50330c60d76a2a6924
Matt <31932179+MattNoName@users.noreply.github.com>
2021-07-17 13:36:09 -0400

    Got rid of no-sync for loadPersistentContainer and made headers blue fro CreateContactCardViewController

--- Contact Cards/Utilities/LoadPersistentContainer.swift
+++ Contact Cards/Utilities/LoadPersistentContainer.swift
@@ -14,14 +14,6 @@ func loadPersistentContainer() -> NSPersistentCloudKitContainer {
 		let storeURL=fileContainerURL.appendingPathComponent("ContactCards.sqlite")
 		let storeDescription=NSPersistentStoreDescription(url: storeURL)
 		storeDescription.cloudKitContainerOptions=NSPersistentCloudKitContainerOptions(containerIdentifier: "iCloud.com.apps.celeritas.ContactCards")
-		#if os(watchOS)
-		#else
-		let keyValueStore=NSUbiquitousKeyValueStore.default
-		if !keyValueStore.bool(forKey: "iCloudSync") {
-			storeDescription.setOption(true as NSNumber, forKey: NSPersistentHistoryTrackingKey)
-			storeDescription.cloudKitContainerOptions=nil
-		}
-		#endif
 		container.persistentStoreDescriptions=[storeDescription]
 	}
 	container.loadPersistentStores { (_, error) in
