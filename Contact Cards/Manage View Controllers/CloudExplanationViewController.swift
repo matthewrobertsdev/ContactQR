52d97dfc8cd2c7cbde2e33b1825899fdaee8b6ca
Matt <31932179+MattNoName@users.noreply.github.com>
2021-07-17 14:14:57 -0400

    Went back to labelColor for contact fields' headings

--- Contact Cards/Manage View Controllers/CloudExplanationViewController.swift
+++ Contact Cards/Manage View Controllers/CloudExplanationViewController.swift
@@ -10,7 +10,7 @@ class CloudExplanationViewController: UIViewController {
 	@IBOutlet weak var explanationLabel: UILabel!
 	override func viewDidLoad() {
         super.viewDidLoad()
-		explanationLabel.text=iCloudExplanationString()+"by using the back button and follwing the steps described on the manage contact cards interface."
+		explanationLabel.text=iCloudExplanationString()+"by using the back button and following the steps described on the manage contact cards interface."
 		explanationLabel.sizeToFit()
         // Do any additional setup after loading the view.
     }
